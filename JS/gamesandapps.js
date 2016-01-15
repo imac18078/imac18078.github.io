2353051dcedbfdf388b7a5acd7a598f55587ede5
Adam <rpihardy@gmail.com>
2016-01-15 15:26:47 -0500

    Update games JS

--- JS/gamesandapps.js
+++ JS/gamesandapps.js
@@ -4,7 +4,7 @@ if (userResponse === "no" || userResponse === "n") {
 	window.location = "../HTML/sorry.html";
 }
 else if (userResponse === "yes" || userResponse === "y"){
-	break;
+
 }
 else {
 	window.location = "../HTML/worthy.html";
