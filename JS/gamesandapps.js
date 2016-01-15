e820c04ac11a45b3472b2c094934ad08f7f98ccd
Adam <rpihardy@gmail.com>
2016-01-15 15:32:02 -0500

    Update games JS

--- JS/gamesandapps.js
+++ JS/gamesandapps.js
@@ -3,6 +3,6 @@ userResponse = prompt("Would you like to play a game?").toLowerCase();
 if (userResponse === "no" || userResponse === "n") {
 	window.location = "../HTML/sorry.html";
 }
-else if (userResponse != "yes" || userResponse != "y") {
+else if (userResponse !== "yes" && userResponse !== "y") {
 	window.location = "../HTML/worthy.html";
 }
